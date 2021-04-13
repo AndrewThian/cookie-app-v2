b18820e0757fdfabe565bb08c111d0b709c83a4e
AndrewThian <andrew@stashaway.com>
2021-04-13 19:34:30 +0800

    feat: add client side routing
    - Support server side urls via next.config.js redirects
    - Use react-router 6 for client side routing

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    webpack5: true
+  },
+  async rewrites() {
+    return [
+      // Do not rewrite API routes
+      {
+        source: '/api/:any*',
+        destination: '/api/:any*',
+      },
+      // Rewrite everything else to use `pages/index`
+      {
+        source: '/:any*',
+        destination: '/',
+      },
+    ]
+  },
+}
