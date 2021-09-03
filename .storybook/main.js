81fed20f022bfb38e02edeb940e5ab831b692131
Andrew Thian <andrew@stashaway.com>
2021-09-03 17:25:51 +0800

    fix typescript error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
     {
       name: '@storybook/addon-postcss',
       options: {
@@ -15,6 +14,10 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()]
+    return config
+  },
   // babel: async (options) => ({
   //   ...options,
   //   plugins: [
