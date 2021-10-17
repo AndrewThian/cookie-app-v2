2ba80c1fa2f2bcd9834a4f61bf1a7b5d42b220f0
Andrew Thian <andrew@stashaway.com>
2021-10-17 20:08:33 +0800

    initial ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 import Home from 'src/ui/Home'
+import SetupYourBudget from 'src/ui/SetupYourBudget'
+
 import { ClientRender } from 'src/ui/system/ClientRender'
 
 function App(): JSX.Element {
@@ -11,6 +12,9 @@ function App(): JSX.Element {
         <Switch>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/setup">
+            <SetupYourBudget />
           </Route>
         </Switch>
       </Router>
---
+++ src/ui/SetupYourBudget/index.tsx
@@ -0,0 +1,7 @@
+import { AppShell, BgColor } from '@components/AppShell'
+
+const SetupYourBudget = () => {
+  return <AppShell bgColor={BgColor.WHITE}>Set up your budget</AppShell>
+}
+
+export default SetupYourBudget
